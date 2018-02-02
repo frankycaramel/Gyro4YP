7434da50332356bc897b44586c53ca24bffc2cfe
frankycaramel <martin.legr@gmail.com>
2018-02-02 12:01:59 +0000

    comment change test

--- Gyro_V1/Gyro_V1.ino
+++ Gyro_V1/Gyro_V1.ino
@@ -5,6 +5,8 @@ int gyro_x, gyro_y, gyro_z;
 long gyro_x_cal, gyro_y_cal, gyro_z_cal;
 boolean set_gyro_angles;
 
+//Git test
+
 long acc_x, acc_y, acc_z, acc_total_vector;
 float angle_roll_acc, angle_pitch_acc;
 
